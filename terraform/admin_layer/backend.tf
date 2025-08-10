7243d7f9f74092f380ae0c4bd05a846ba6ca7e4c
ulfiac <221777317+ulfiac@users.noreply.github.com>
2025-08-10 17:02:03 -0400

    adjust admin_layer backend key

--- terraform/admin_layer/backend.tf
+++ terraform/admin_layer/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket       = "ulfiac-terraform-state"
     encrypt      = true
-    key          = "aws-init/terraform.tfstate"
+    key          = "aws-bootstrap/admin_layer/us-east-2/terraform.tfstate"
     region       = "us-east-2"
     use_lockfile = true
   }
