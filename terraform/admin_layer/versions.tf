80991bc129527e480be75d6afc23b94563d1622c
ulfiac <221777317+ulfiac@users.noreply.github.com>
2025-08-12 09:06:34 -0400

    adjust dependabot

--- terraform/admin_layer/versions.tf
+++ terraform/admin_layer/versions.tf
@@ -2,8 +2,8 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 6.5.0"
+      version = "6.5.0"
     }
   }
-  required_version = ">= 1.12.0"
+  required_version = "1.12.0"
 }
