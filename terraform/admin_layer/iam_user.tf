71c13bfc745de956126d01cc66239576aa2d9f67
ulfiac <221777317+ulfiac@users.noreply.github.com>
2025-08-14 08:21:27 -0400

    add admin user

--- terraform/admin_layer/iam_user.tf
+++ terraform/admin_layer/iam_user.tf
@@ -1,1 +1,13 @@
-# placeholder
+resource "aws_iam_user" "admin_user" {
+  name          = "ulfiac"
+  force_destroy = true
+}
+
+data "aws_iam_policy" "admin_access" {
+  name = "AdministratorAccess"
+}
+
+resource "aws_iam_user_policy_attachment" "admin_user" {
+  user       = aws_iam_user.admin_user.name
+  policy_arn = aws_iam_policy.admin_access.arn
+}
