4107412c1ab4ebfe0bcbd557d903a5209a3ca9c1
ulfiac <221777317+ulfiac@users.noreply.github.com>
2025-08-15 19:39:54 -0400

    maybe without the -n

--- scripts/add_gpg_public_key.sh
+++ scripts/add_gpg_public_key.sh
@@ -9,13 +9,9 @@ EXPECTED_SHA256_BINARY='55576a9b920fa828647f1866c69c8936138f3c97a1f62875ed721269
 EXPECTED_SHA256_BINARY_BASE64='c56a7fc499669c37391f61b72c796a60fc5bb01ee88bf5da25547684c1a438a3  ./public_key_binary_base64encoded.gpg'
 EXPECTED_SHA256_ARMORED='324a45b8e9456f7506dbe5bf6c3d491e98bced1e2bf9d6992e23d658a2f56e34  ./public_key_armored.asc'
 
-# get version
-shasum --version
-gpg --version
-
 # echo environment var into file(s)
-echo -n $GPG_PUBLIC_KEY_BINARY_BASE64 > ./public_key_binary_base64encoded.gpg
-echo -n $GPG_PUBLIC_KEY_ARMORED > ./public_key_armored.asc
+echo $GPG_PUBLIC_KEY_BINARY_BASE64 > ./public_key_binary_base64encoded.gpg
+echo $GPG_PUBLIC_KEY_ARMORED > ./public_key_armored.asc
 
 # calculate sha256 hash of file(s)
 calculated_sha256_binary_base64=$(shasum -a 256 ./public_key_binary_base64encoded.gpg)
