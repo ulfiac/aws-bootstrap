f0dd62d51381ca8c987d1090737be87b81f95ea2
ulfiac <221777317+ulfiac@users.noreply.github.com>
2026-04-03 18:22:08 -0400

    remove organization

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,8 +2,3 @@ variable "oidc_role_to_assume" {
   description = "The name of the IAM role to be assumed by OIDC-auth'ed GitHub Actions."
   type        = string
 }
-
-variable "aws_environment" {
-  description = "The name of the GitHub environment for the AWS account."
-  type        = string
-}
