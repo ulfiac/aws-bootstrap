f6df8dac50b3227641ccdd0e362a8af6192269f4
ulfiac <221777317+ulfiac@users.noreply.github.com>
2026-06-18 09:03:20 -0400

    adjust find-dir for terraform-docs

---
+++ terraform/modules/tf_state_bucket/variables.tf
@@ -0,0 +1,5 @@
+variable "test_terraform_docs_tf_state_bucket_module" {
+  description = "Whether to test the terraform-docs generation for the tf_state_bucket module. This is useful to ensure that the documentation is up-to-date and correctly generated."
+  type        = bool
+  default     = true
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,9 @@ variable "oidc_role_to_assume" {
   description = "The name of the IAM role to be assumed by OIDC-auth'ed GitHub Actions."
   type        = string
 }
+
+variable "test_terraform_docs_bootstrap_module" {
+  description = "Whether to test the terraform-docs generation for the bootstrap module. This is useful to ensure that the documentation is up-to-date and correctly generated."
+  type        = bool
+  default     = true
+}
