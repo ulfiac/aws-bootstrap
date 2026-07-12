fc8b413329cbafa7a30d50529e82782d1347bffb
ulfiac <221777317+ulfiac@users.noreply.github.com>
2026-07-12 15:07:37 -0400

    set version

--- terraform/modules/tf_state_bucket/versions.tf
+++ terraform/modules/tf_state_bucket/versions.tf
@@ -2,8 +2,8 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 6.51.0" # current as of 2026-06-18
+      version = "6.51.0"
     }
   }
-  required_version = ">= 1.15.6" # current as of 2026-06-18
+  required_version = "1.15.6"
 }
